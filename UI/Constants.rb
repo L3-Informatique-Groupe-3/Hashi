37746951755a3a71325f728645262a0d26c336f4
Checkam <maxencedespres@gmail.com>
2020-02-11 18:03:07 +0100

    Ajout des contantes

---
+++ UI/Constants.rb
@@ -0,0 +1,18 @@
+# @Author: Despres Maxence
+# @Date:  11-Feb-2020
+# @Email:  maxence.despres.etu@univ-lemans.fr
+# @Filename: Constants.rb
+# @Last modified by:   Checkam
+# @Last modified time: 11-Feb-2020
+
+
+
+
+module Constants
+	SPACING = 0
+	SCREEN=Gdk::Screen.default
+	BUTTON_PADDING=SCREEN.height*0.03
+	BUTTON_SIZE=SCREEN.height*0.04
+	TITTLE_SIZE=SCREEN.height*0.08
+	MESSAGE_SIZE=SCREEN.height*0.02
+end
