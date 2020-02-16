8ccc8131db654f64e26f2909907ca72f6765035d
Checkam <maxencedespres@gmail.com>
2020-02-16 17:32:27 +0100

    Ajout des constantes de gestion du click - LEFT - MIDDLE - RIGHT - CURSORIN - CURSOROUT

---
+++ UI/Click.rb
@@ -0,0 +1,16 @@
+# @Author: Despres Maxence
+# @Date:   07-Feb-2020
+# @Email:  maxence.despres.etu@univ-lemans.fr
+# @Filename: Click.rb
+# @Last modified by:   checkam
+# @Last modified time: 16-Feb-2020
+
+
+
+module Click # :nodoc:
+	LEFT = 1
+	MIDDLE = 2
+	RIGHT = 3
+  CURSORIN=Gdk::Cursor.new(:hand1)
+  CURSOROUT=Gdk::Cursor.new(:arrow)
+end
