39ad111285505e28ccc2878969392eeef6afccb2
Checkam <maxencedespres@gmail.com>
2020-02-16 16:34:59 +0100

    Ajout commentaire sur la class Screen

--- UI/Screen/Screen.rb
+++ UI/Screen/Screen.rb
@@ -1,9 +1,9 @@
-# @Author: Despres Maxence
+# @Author: Despres Maxence <checkam>
 # @Date:   11-Feb-2020
 # @Email:  maxence.despres.etu@univ-lemans.fr
-# @Filename: Screen.rb
-# @Last modified by:   Checkam
-# @Last modified time: 11-Feb-2020
+# @Filename: GameScreen.rb
+# @Last modified by:   checkam
+# @Last modified time: 16-Feb-2020
 
 
 
@@ -11,20 +11,50 @@
 require 'gtk3'
 require File.dirname(__FILE__) + "/../Constants"
 
-
+##
+# ===== Presentation
+#   Screen is a abstract class needed to display all screen
+#
+# ===== Variables
+#    
+#   @gtkObject
+#   @backgroundPath
+#   @parent
+#   @buffer
+#
+# ===== Methods
+#
+#   new initialization method
+#   applyOn Use to display something on a screen
+#
 class Screen
   @gtkObject
   @backgroundPath
+  @parent
+  @buffer
 
-  def initialize(parent)
+  ##
+  # The class' constructor.
+  #
+  # ===== Attributes
+  # * +parent+ -
+  # * +background+ - default value "/../../../Assets/Backgrounds/fond_low_poly.png"
+  #
+  #-------------------------------------------------
+  def initialize(parent, background="/../../../Assets/Backgrounds/fond_low_poly.png")
     screen = Constants::SCREEN
     @parent=parent
-  	@buffer = GdkPixbuf::Pixbuf.new(file: File.dirname(__FILE__) + "/../../../Assets/Backgrounds/fond_low_poly.png")
+  	@buffer = GdkPixbuf::Pixbuf.new(file: File.dirname(__FILE__) + background)
     @buffer=@buffer.scale(screen.width,screen.height)
-  	@buffer2 = GdkPixbuf::Pixbuf.new(file: File.dirname(__FILE__) + "/../../../Assets/Backgrounds/fond_low_poly.png")
-    @buffer2=@buffer2.scale(screen.width,screen.height)
   end
 
+  ##
+  # Use to display something on a screen
+  #
+  # ===== Attributes
+  # * +widget+ - widget to display on this screen
+  #
+  #-------------------------------------------------
   def applyOn(widget)
     widget.each { |child|
       widget.remove(child)
