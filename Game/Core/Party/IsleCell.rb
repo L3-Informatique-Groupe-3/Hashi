989afcbac54de66308d8b30ff59f196ab7e30527
jashbin <jasmin.galbrun@gmail.com>
2020-02-25 15:02:05 +0100

    Création des différentes cellules

--- Game/Core/Party/IsleCell.rb
+++ Game/Core/Party/IsleCell.rb
@@ -1,4 +1,14 @@
-# Create 19/02/2020
+################################################################################
+# File: IsleCell.rb                                                            #
+# Project: Hashi                                                               #
+# File Created: Monday, 19th February 2020 11:00:27 am                         #
+# Author: <jashbin>Galbrun J                                                   #
+# -----                                                                        #
+# Last Modified: Monday, 24th February 2020 5:17:27 pm                         #
+# Modified By: <jashbin>Galbrun J                                              #
+################################################################################
+
+require "./Cell"
 
 ##
 # ===== Presentation
@@ -8,7 +18,7 @@
 # * +bridgeNumber+ - Number of bridge which must be connected
 #
 # ===== Methods
-# * +getBridgeNumber+ - return the number of bridge which must be connected (alias IsleCell#BridgeNumber)
+# * +getBridgeNumber+ - return the number of bridge which must be connected (alias IsleCell#bridgeNumber)
 ##
 class IsleCell < Cell
     @bridgeNumber
@@ -23,8 +33,8 @@ class IsleCell < Cell
     # The class' constructor.
     # Init +state+ to :isle and +alterable+ to false by default
     # ---
-    def initialize
+    def initialize(bridgeNb)
         @state = :isle
-        @alterable = false
+        @bridgeNumber = bridgeNb
     end
 end
