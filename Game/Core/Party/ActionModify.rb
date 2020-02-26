f8e1ecc50c5bbf40717c96ad7f67b0cb2528227e
Adrali <lemaitre.pierre@gmail.com>
2020-02-26 18:50:17 +0100

    Ajout structure actions et contenu de applyAction

--- Game/Core/Party/ActionModify.rb
+++ Game/Core/Party/ActionModify.rb
@@ -1,5 +1,5 @@
 ################################################################################
-# File: Action.rb                                                                #
+# File: ActionModify.rb                                                                #
 # Project: Hashi                                                               #
 # File Created: Tuesday, 20th February 2020 11:00:27 am                        #
 # Author: <Adrali>Lemaitre P                                                   #
@@ -17,10 +17,12 @@
 # * +y+ - y coordonate of the cell
 # * +grid+ - Store the grid where the action will be applied
 # ===== Methods
-# * +applyAction+ - Apply an action
-# * +applyOpposite+ - Apply the opposite action
+# * +applyAction+ - Modify an existing bridge
+# * +applyOpposite+ - Modify a bridge as the opposite of the action
 ##
 class ActionCreate
+    attr_reader :x,:y,:grid
+
     ##
     # The class' constructor.
     def initialize(x,y,grid)
@@ -30,17 +32,18 @@ class ActionCreate
     end
 
     ##
-    # Apply the action
+    # Create a bridge between two islands
     #
     # ===== Return
     # return true
     # ---
     def applyAction
+        grid.changeBridge(@x,@y)
         return true
     end
 
     ##
-    # Apply the opposite action
+    # Destroy a bridge between two islands
     #
     # ===== Return
     # return true
