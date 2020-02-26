381d9fbc6322dd00a3f9a07e1623a1037575f5a2
Adrali <lemaitre.pierre@gmail.com>
2020-02-26 18:50:17 +0100

    Ajout structure actions et contenu de applyAction

--- Game/Core/Party/ActionCreate.rb
+++ Game/Core/Party/ActionCreate.rb
@@ -1,5 +1,5 @@
 ################################################################################
-# File: Action.rb                                                                #
+# File: ActionCreate.rb                                                                #
 # Project: Hashi                                                               #
 # File Created: Tuesday, 20th February 2020 11:00:27 am                        #
 # Author: <Adrali>Lemaitre P                                                   #
@@ -13,34 +13,41 @@
 #   Cell is a class that define the cell's behaviors
 #
 # ===== Variables
-# * +x+ - x coordonate of the cell
-# * +y+ - y coordonate of the cell
+# * +x1+ - x coordonate of the first cell
+# * +y1+ - y coordonate of the first cell
+# * +x2+ - x coordonate of the second cell
+# * +y2+ - y coordonate of the second cell
 # * +grid+ - Store the grid where the action will be applied
 # ===== Methods
-# * +applyAction+ - Apply an action
-# * +applyOpposite+ - Apply the opposite action
+# * +applyAction+ - Create a bridge between two islands
+# * +applyOpposite+ - Destroy a bridge between two islands
 ##
 class ActionCreate
+    attr_reader :x1,:y1,:x2,:y2,:grid
+
     ##
     # The class' constructor.
-    def initialize(x,y,grid)
-        @x = x
-        @y = y
+    def initialize(x1,y1,x2,y2,grid)
+        @x1 = x1
+        @y1 = y1
+        @x2 = x2
+        @y2 = y2
         @grid = grid
     end
 
     ##
-    # Apply the action
+    # Create a bridge between two islands
     #
     # ===== Return
     # return true
     # ---
     def applyAction
+        grid.createBridge(@x1,@y1, @x2, @y2)
         return true
     end
 
     ##
-    # Apply the opposite action
+    # Destroy a bridge between two islands
     #
     # ===== Return
     # return true
