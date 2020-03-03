3277d56442ea81186c25051a558d44994ec0d9a3
Lemaitre Pierre <s156230@ic2s112-02.univ-lemans.fr>
2020-03-03 17:43:26 +0100

    Update History et Hypothesis

--- Game/Core/Party/History.rb
+++ Game/Core/Party/History.rb
@@ -44,8 +44,39 @@ class History
     
 
     def newHypothesis
-        @hypotheses -= @hypotheses.drop(index + 1)
-
+        @hypotheses -= @hypotheses.drop(@index + 1)
+        @index+=1
         @hypotheses.push(Hypothesis.new(@grid.getCurrentGrid(), :created))
     end
+
+    def validateHypothesis
+        @hypotheses -= @hypotheses.drop(@index + 1)
+        @index+=1
+        @hypotheses.push(Hypothesis.new(@grid.getCurrentGrid(), :validated))
+    end
+
+    def refuteHypothesis
+        if(@hypotheses[@index].type != :validated)
+            @grid.loadCurrentGrid(@hypotheses[@index].saveGrid)
+            @index -=1
+        end
+    end
+
+    def addAction(action)
+        @hypotheses[index].addAction(action)
+    end
+
+    # def undo()
+    #     @hypotheses[index].undo()
+    # end
+
+    # def redo()
+    #     @hypotheses[index].redo()
+    # end
 end
+
+
+# h = History.new(Grid.new("9x9:2c3-1-1c3a-b-a3d3b4d4-aa-a3a3dd5a-2a3a--b1-a3b3cc6dd4b3a--a-1a3a2c1a-a1a2c2c3c3c2"))
+# h.newHypothesis
+# h.refuteHypothesis
+# h.validateHypothesis
