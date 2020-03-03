215077d8c5e8583ec6899ed39d9fb73d90825a95
Lemaitre Pierre <s156230@ic2s112-02.univ-lemans.fr>
2020-03-03 16:46:52 +0100

    Finalisation des actions

--- Game/Core/Party/ActionCreate.rb
+++ Game/Core/Party/ActionCreate.rb
@@ -10,49 +10,61 @@
 
 ##
 # ===== Presentation
-#   Cell is a class that define the cell's behaviors
+# ActionCreate is an Action.
+# Represents the action of the player when he create a bridge between to IslandCells
 #
 # ===== Variables
 # * +x1+ - x coordonate of the first cell
 # * +y1+ - y coordonate of the first cell
 # * +x2+ - x coordonate of the second cell
 # * +y2+ - y coordonate of the second cell
-# * +grid+ - Store the grid where the action will be applied
 # ===== Methods
 # * +applyAction+ - Create a bridge between two islands
 # * +applyOpposite+ - Destroy a bridge between two islands
 ##
-class ActionCreate
-    attr_reader :x1,:y1,:x2,:y2,:grid
+class ActionCreate < Action
+    @x1
+    @y1
+    @x2
+    @y2
+
+    attr_reader :x1,:y1,:x2,:y2,:grid #:nodoc:
 
     ##
     # The class' constructor.
-    def initialize(x1,y1,x2,y2,grid)
+    #
+    # ===== Attributes
+    # * +grid+ - The grid where the action will be applied 
+    # * +x1+ - x coordonate of the first cell
+    # * +y1+ - y coordonate of the first cell
+    # * +x2+ - x coordonate of the second cell
+    # * +y2+ - y coordonate of the second cell
+    # ---
+    def initialize(grid, x1, y1, x2, y2)
+        super(grid)
         @x1 = x1
         @y1 = y1
         @x2 = x2
         @y2 = y2
-        @grid = grid
     end
 
     ##
     # Create a bridge between two islands
     #
     # ===== Return
-    # return true
+    # return true if the action have been made, else false
     # ---
     def applyAction
-        grid.createBridge(@x1,@y1, @x2, @y2)
-        return true
+        return grid.createBridge(@x1,@y1, @x2, @y2)
     end
 
     ##
     # Destroy a bridge between two islands
     #
     # ===== Return
-    # return true
+    # return true if the action have been made, else false
     # ---
     def applyOpposite
-        return true
+        return grid.removeBridge(@x1, @y1, @x2, @y2)
     end
 end
