4c07b04e7aa4a5489f618e8ae5e8b8bf74f7b332
Checkam <maxencedespres@gmail.com>
2020-04-03 17:25:53 +0200

    Modification taille des cellules du jeu

--- Game/UI/AssetsClass/CellAsset.rb
+++ Game/UI/AssetsClass/CellAsset.rb
@@ -3,7 +3,7 @@
 # @Email:  maxence.despres.etu@univ-lemans.fr
 # @Filename: CellAsset.rb
 # @Last modified by:   checkam
-# @Last modified time: 16-Feb-2020
+# @Last modified time: 03-Apr-2020
 
 
 
@@ -14,7 +14,7 @@ require File.dirname(__FILE__) + "/../Constants"
 # ===== Presentation
 # CellAsset is a herite class of Asset needed for load assets just for cells
 #
-#	@buffer		image 
+#	@buffer		image
 # @nRows 		number of rows of grid
 # @nCols		number of cols of grid
 #
@@ -48,8 +48,8 @@ class CellAsset < Asset
 	#
 	def resize
 			screen = Gdk::Screen.default
-			m1=((screen.width*0.7 ) / (@nCols+1)) - Constants::SPACING
-			m2=((screen.height*1.0) / (@nRows+1)) - Constants::SPACING
+			m1=((screen.width*0.7 ) / (@nCols+1))
+			m2=((screen.height*0.90) / (@nRows+1))
 			mf= m1 < m2 ? m1 : m2
 			super(mf,mf)
 		end
