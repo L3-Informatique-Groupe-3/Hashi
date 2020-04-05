954886967afb3ef8b5411bb5fddb4a7e1b270c54
Checkam <maxencedespres@gmail.com>
2020-04-05 17:25:24 +0200

    Modifiction title + Fix : - fix de la couleur - Ajout d'un sous texte

--- Game/UI/Component/Titre.rb
+++ Game/UI/Component/Titre.rb
@@ -3,7 +3,7 @@
 # @Email:  maxence.despres.etu@univ-lemans.fr
 # @Filename: Titre.rb
 # @Last modified by:   checkam
-# @Last modified time: 16-Feb-2020
+# @Last modified time: 05-Apr-2020
 
 require_relative "./Text"
 
@@ -15,23 +15,24 @@ class Titre < Text
       super(label:label, size:size, padding: padding, width:width, height: height)
       @weight = "ultrabold"
       @font = "arial"
-      @color = "empty"
+      @color = "white"
       @size = Constants::TITTLE_SIZE
       apply
 
-      #underText = Text.new(label:label, size: Constants::TITTLE_SIZE+2)
-      #underText.font="arial"
-      #underText.color="black"
-      #underText.weight="bold"
-      #underText.style="normal"
-      #underText.apply
+      underText = Text.new(label:label, size: Constants::TITTLE_SIZE+2)
+      underText.font="arial"
+      underText.color="black"
+      underText.weight="bold"
+      underText.style="normal"
+      underText.apply
 
       @gtkObject.each { |child|
         @gtkObject.remove(child)
       }
-      table=Gtk::Table.new(3,3)
-      table.attach(@eventBox, 1, 2, 1, 2)
-      #table.attach(underText.gtkObject, 0, 3, 0, 3)
+      table=Gtk::Table.new(1,1)
+
+      table.attach(@eventBox, 0, 1, 0, 1)
+      table.attach(underText.gtkObject, 0, 1, 0, 1 )
       @gtkObject.add(table)
     end
 
