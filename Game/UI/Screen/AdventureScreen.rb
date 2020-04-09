10999c6d01ef1afdfd9830e8851811321e634b89
Checkam <maxencedespres@gmail.com>
2020-04-09 17:03:32 +0200

    Modification AdventureScreen : - Liaison avec la BDD
    Co-Authored-By: jashbin <jasmin.galbrun@gmail.com>

--- Game/UI/Screen/AdventureScreen.rb
+++ Game/UI/Screen/AdventureScreen.rb
@@ -13,35 +13,46 @@ class AdventureScreen < Screen
   def initialize(win,uiManager)
     super(win,"/../../../Assets/Backgrounds/worldmap.png")
     @gtkObject =  Gtk::Table.new(4,4)
+
+    @win = win
+    @uiManager = uiManager
+
     pathAssets=File.dirname(__FILE__) + "/../../../Assets/"
     screen=Gdk::Screen.default
-
+    @save=nil
     backToMenuButton = Button.new(label:"Menu Principal", width: screen.width*0.1,height: screen.height*0.08, size: 20)
     backToMenuButton.onClick(){
       uiManager.mainmenu.applyOn(win)
     }
     @gtkObject.attach( Gtk::Alignment.new(0.05, 0.95, 0, 0).add(backToMenuButton.gtkObject),0,4,0,4)
 
-    countryButton = Hash.new
-    countryButton["groenland"] = ButtonShape.new(shape: pathAssets + "ShapeButton/groenland.png", width: screen.width, height: screen.height)
-    countryButton["afrique"] = ButtonShape.new(shape: pathAssets + "ShapeButton/afrique.png", width: screen.width, height: screen.height)
-    countryButton["amerique_sud"] = ButtonShape.new(shape: pathAssets + "ShapeButton/amerique_sud.png", width: screen.width, height: screen.height)
-    countryButton["asie"] = ButtonShape.new(shape: pathAssets + "ShapeButton/asie.png", width: screen.width, height: screen.height)
-    countryButton["europe"] = ButtonShape.new(shape: pathAssets + "ShapeButton/europe.png", width: screen.width, height: screen.height)
-    countryButton["australie"] = ButtonShape.new(shape: pathAssets + "ShapeButton/australie.png", width: screen.width, height: screen.height)
-    countryButton["us"] = ButtonShape.new(shape: pathAssets + "ShapeButton/us.png", width: screen.width, height: screen.height)
+    @countryButton = Hash.new
+    # @countryButton["groenland"] = ButtonShape.new(shape: pathAssets + "ShapeButton/groenland.png", width: screen.width, height: screen.height)
+    # @countryButton["afrique"] = ButtonShape.new(shape: pathAssets + "ShapeButton/afrique.png", width: screen.width, height: screen.height)
+    @countryButton["amerique_sud"] = ButtonShape.new(shape: pathAssets + "ShapeButton/amerique_sud.png", width: screen.width, height: screen.height)
+    @countryButton["asie"] = ButtonShape.new(shape: pathAssets + "ShapeButton/asie.png", width: screen.width, height: screen.height)
+    @countryButton["europe"] = ButtonShape.new(shape: pathAssets + "ShapeButton/europe.png", width: screen.width, height: screen.height)
+    @countryButton["australie"] = ButtonShape.new(shape: pathAssets + "ShapeButton/australie.png", width: screen.width, height: screen.height)
+    @countryButton["us"] = ButtonShape.new(shape: pathAssets + "ShapeButton/us.png", width: screen.width, height: screen.height)
 
     manager = ButtonShapeManager.new
     @gtkObject.attach(manager.gtkObject,0,4,0,4)
 
-    countryButton.each_key { |key|
-          @gtkObject.attach(countryButton[key].gtkObject,0,4,0,4)
-          countryButton[key].setAction(lambda { AdventureLevelSelectionScreen.new(window:win,uiManager:uiManager).applyOn(win) })
-          manager.addButton(countryButton[key])
+    @countryButton.each_key { |key|
+          @gtkObject.attach(@countryButton[key].gtkObject,0,4,0,4)
+          manager.addButton(@countryButton[key])
     }
 
 
     @gtkObject.attach(Gtk::Image.new(pixbuf: @buffer),0,4,0,4)
+  end
+
+  def setSave(save)
+    @save = save
+    keys = @countryButton.keys
+    keys.each_index { |i|
+      @countryButton[keys[i]].setAction(lambda { AdventureLevelSelectionScreen.new(window:@win,uiManager:@uiManager, save: @save, countryNumber: i + 1).applyOn(@win)})
+    }
   end
 
 end
