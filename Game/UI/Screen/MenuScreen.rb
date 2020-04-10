2605d672a61edf916d2125e40c13b9675c00c1ea
Checkam <maxencedespres@gmail.com>
2020-04-10 16:39:54 +0200

    Modification du MenuScreen - Prends 3 ou moins boutons désormais

--- Game/UI/Screen/MenuScreen.rb
+++ Game/UI/Screen/MenuScreen.rb
@@ -13,33 +13,45 @@ require File.dirname(__FILE__) + "/Screen"
 class MenuScreen < Screen
     @gtkObject
 
-    def initialize(window:win,title:"Title",button1:"Button 1",button2:"Button 2",button3:"Button 3",buttonAction1:nil,buttonAction2:nil,buttonAction3:nil,haveBackButton:false,uiManager:nil)
+    def initialize(window:win,title:"",button1:"",button2:"",button3:"",buttonAction1:nil,buttonAction2:nil,buttonAction3:nil,haveBackButton:false,uiManager:nil)
         super(window,"/../../../Assets/Backgrounds/fond-naturel.png")
-        @gtkObject = Gtk::Table.new(4,4)
         screen=Gdk::Screen.default
 
-        menuTitle=Titre.new(label:title)
 
-        b1=Button.new(label:button1)
-        b1.onClick(){
-          buttonAction1.call if buttonAction1 != nil
-        }
 
-        b2=Button.new(label:button2)
-        b2.onClick(){
-          buttonAction2.call if buttonAction2 != nil
-        }
-
-        b3=Button.new(label:button3)
-        b3.onClick(){
-          buttonAction3.call if buttonAction3 != nil
-        }
-
+        @gtkObject = Gtk::Table.new(4,4)
         globalBox = Gtk::Box.new(:vertical)
-        globalBox.pack_start(menuTitle.gtkObject, expand: false, fill: false, padding: 50)
-        globalBox.pack_start(b1.gtkObject, expand: false, fill: false, padding: 50)
-        globalBox.pack_start(b2.gtkObject, expand: false, fill: false, padding: 50)
-        globalBox.pack_start(b3.gtkObject, expand: false, fill: false, padding: 50)
+        if title != ""
+          menuTitle=Titre.new(label:title)
+          globalBox.pack_start(menuTitle.gtkObject, expand: false, fill: false, padding: 50)
+        end
+
+        if button1 != ""
+          b1=Button.new(label:button1)
+          b1.onClick(){
+            buttonAction1.call if buttonAction1 != nil
+          }
+          globalBox.pack_start(b1.gtkObject, expand: false, fill: false, padding: 50)
+        end
+
+        if button2 != ""
+          b2=Button.new(label:button2)
+          b2.onClick(){
+            buttonAction2.call if buttonAction2 != nil
+          }
+          globalBox.pack_start(b2.gtkObject, expand: false, fill: false, padding: 50)
+        end
+
+        if button3 != ""
+          b3=Button.new(label:button3)
+          b3.onClick(){
+            buttonAction3.call if buttonAction3 != nil
+          }
+          globalBox.pack_start(b3.gtkObject, expand: false, fill: false, padding: 50)
+        end
+
+
+
 
         if haveBackButton == true
           backToButton = Button.new(label:"Menu Principal", width: screen.width*0.1,height: screen.height*0.08, size: 20)
